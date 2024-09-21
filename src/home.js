1d8d99e2f4da7a871e620d1f39442ff12d6072a2
Herciu Marian <herciumarian8@gmail.com>
2024-09-21 19:09:06 +0300

    Js for rendering the home page

---
+++ src/home.js
@@ -0,0 +1,8 @@
+
+const container = document.querySelector("#content");
+
+const body = document.querySelector("body");
+
+export default function homePage() {
+    console.log("rendering the hoome page");
+}
